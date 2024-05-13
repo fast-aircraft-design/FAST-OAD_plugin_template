1896091a259f9a5338fe59b57e022c3a1c01ce5a
Christophe DAVID <49477099+christophe-david@users.noreply.github.com>
2024-05-13 11:25:32 +0200

    Updated code

--- src/rename_me/models/sample/tests/test_sample_discipline.py
+++ src/rename_me/models/sample/tests/test_sample_discipline.py
@@ -1,22 +1,7 @@
-"""
-Test module for sample discipline
-"""
-#  This file is part of FAST : A framework for rapid Overall Aircraft Design
-#  Copyright (C) 2020  ONERA & ISAE-SUPAERO
-#  FAST is free software: you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation, either version 3 of the License, or
-#  (at your option) any later version.
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#  You should have received a copy of the GNU General Public License
-#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""Test module for sample discipline"""
 
-import os.path as pth
-from os import mkdir
-from shutil import rmtree
+import shutil
+from pathlib import Path
 
 import fastoad.api as oad
 import pytest
@@ -24,23 +9,21 @@ from fastoad._utils.testing import run_system
 
 from ..sample_discipline import SampleDiscipline
 
-DATA_FOLDER_PATH = pth.join(pth.dirname(__file__), "data")
-RESULTS_FOLDER_PATH = pth.join(
-    pth.dirname(__file__), "results", pth.splitext(pth.basename(__file__))[0]
-)
+DATA_FOLDER_PATH = Path(__file__).parent / "data"
+RESULTS_FOLDER_PATH = Path(__file__).parent / "results" / Path(__file__).stem
 
 
 @pytest.fixture(scope="module")
 def cleanup():
-    rmtree(RESULTS_FOLDER_PATH, ignore_errors=True)
-    mkdir(RESULTS_FOLDER_PATH)
+    shutil.rmtree(RESULTS_FOLDER_PATH, ignore_errors=True)
+    RESULTS_FOLDER_PATH.mkdir()
 
 
 def test_sample_discipline():
     """Tests computation of the sample discipline."""
 
     # Research independent input value in .xml file
-    ivc = oad.DataFile(pth.join(DATA_FOLDER_PATH, "data.xml")).to_ivc()
+    ivc = oad.DataFile((DATA_FOLDER_PATH / "data.xml").as_posix()).to_ivc()
 
     # Run problem and check obtained value(s) is/(are) correct
     problem = run_system(SampleDiscipline(), ivc)
--- tests/integration_tests/test_sample_process.py
+++ tests/integration_tests/test_sample_process.py
@@ -1,31 +1,23 @@
-# This file is part of FAST-OAD_CS23-HE : A framework for rapid Overall Aircraft Design of Hybrid
-# Electric Aircraft.
-# Copyright (C) 2022 ISAE-SUPAERO
-
-import os.path as pth
+import shutil
 from os import mkdir
-from shutil import rmtree
+from pathlib import Path
 
 import fastoad.api as oad
 import pytest
 
-DATA_FOLDER_PATH = pth.join(pth.dirname(__file__), "data")
-RESULTS_FOLDER_PATH = pth.join(
-    pth.dirname(__file__), "results", pth.splitext(pth.basename(__file__))[0]
-)
+DATA_FOLDER_PATH = Path(__file__).parent / "data"
+RESULTS_FOLDER_PATH = Path(__file__).parent / "results" / Path(__file__).stem
 
 
 @pytest.fixture(scope="module")
 def cleanup():
-    rmtree(RESULTS_FOLDER_PATH, ignore_errors=True)
+    shutil.rmtree(RESULTS_FOLDER_PATH, ignore_errors=True)
     mkdir(RESULTS_FOLDER_PATH)
 
 
 def test_sample_process():
-    configurator = oad.FASTOADProblemConfigurator(pth.join(DATA_FOLDER_PATH, "conf.yml"))
-    ref_inputs = pth.join(DATA_FOLDER_PATH, "data.xml")
-
-    oad.DataFile(ref_inputs)
+    configurator = oad.FASTOADProblemConfigurator((DATA_FOLDER_PATH / "conf.yml").as_posix())
+    ref_inputs = (DATA_FOLDER_PATH / "data.xml").as_posix()
 
     # Create problem with inputs
     problem = configurator.get_problem()
